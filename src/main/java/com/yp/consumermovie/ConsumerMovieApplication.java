fcca695697dd5755d272647ce7fd66b221bb40f6
IMYP <1046243253@qq.com>
2019-09-29 23:45:55 +0800

    feign

--- src/main/java/com/yp/consumermovie/ConsumerMovieApplication.java
+++ src/main/java/com/yp/consumermovie/ConsumerMovieApplication.java
@@ -1,9 +1,19 @@
 package com.yp.consumermovie;
 
+import feign.Contract;
+import feign.RequestTemplate;
+import feign.codec.Decoder;
+import feign.codec.EncodeException;
+import feign.codec.Encoder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
 
 @SpringBootApplication
 public class ConsumerMovieApplication {
@@ -13,8 +23,8 @@ public class ConsumerMovieApplication {
 	}
 	
 	@Bean
+	@LoadBalanced
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-
 }
--- src/main/java/com/yp/consumermovie/controller/MovieController.java
+++ src/main/java/com/yp/consumermovie/controller/MovieController.java
@@ -1,6 +1,19 @@
 package com.yp.consumermovie.controller;
 
+import com.yp.consumermovie.feign.UserFeignClient;
+import feign.Client;
+import feign.Contract;
+import feign.Feign;
+import feign.auth.BasicAuthRequestInterceptor;
+import feign.codec.Decoder;
+import feign.codec.Encoder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
+import org.springframework.cloud.netflix.feign.FeignClientsConfiguration;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,13 +21,41 @@ import org.springframework.web.client.RestTemplate;
 
 import com.yp.consumermovie.entity.User;
 
+@Import(FeignClientsConfiguration.class)
 @RestController
 public class MovieController {
 	@Autowired
 	private RestTemplate restTemplate;
+	private UserFeignClient userUserFeignClient;
+	private UserFeignClient adminUserFeignClient;
+//	@Autowired
+//	private LoadBalancerClient loadBalancerClient;
+//	private static final Logger LOGGER  = LoggerFactory.getLogger(MovieController.class);
 
-	@GetMapping("/user/{id}")
-	public User findById(@PathVariable Long id) {
-		return this.restTemplate.getForObject("http://localhost:8000/" + id, User.class);
+	@Autowired
+	public MovieController(Decoder decoder, Encoder encoder, Client client, Contract contract){
+		this.userUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
+				.requestInterceptor(new BasicAuthRequestInterceptor("user","password1")).target(UserFeignClient.class,"http://microservice-simple-provider-user/");
+		this.adminUserFeignClient = Feign.builder().client(client).encoder(encoder).decoder(decoder).contract(contract)
+				.requestInterceptor(new BasicAuthRequestInterceptor("admin","password2")).target(UserFeignClient.class,"http://microservice-simple-provider-user/");
 	}
+
+
+	@GetMapping("/user-user/{id}")
+	public User findById(@PathVariable Long id) {
+//		return this.restTemplate.getForObject("http://microservice-simple-provider-user/" + id, User.class);
+		return this.userUserFeignClient.findById(id);
+	}
+
+	@GetMapping("/user-admin/{id}")
+	public User findByAdmin(@PathVariable Long id){
+		return this.adminUserFeignClient.findById(id);
+	}
+//
+//	@GetMapping("/log-user-instance")
+//	public void logUserInstance(){
+//		ServiceInstance serviceInstance = this.loadBalancerClient.choose("microservice-simple-provider-user");
+//		MovieController.LOGGER.info("{},{},{}",serviceInstance.getServiceId(),serviceInstance.getHost(),serviceInstance);
+//	}
+
 }
